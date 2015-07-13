fbd7ffb433d6b71101cf1b8bd7ede071346da3d8
Laura Walpole <l.a.walpole@googlemail.com>
2015-07-13 10:19:12 +0100

    item added to cart

--- Shop_app/app/controllers/order_products_controller.rb
+++ Shop_app/app/controllers/order_products_controller.rb
@@ -1,4 +1,5 @@
 class OrderProductsController < ApplicationController
+  before_action :set_order_product, only: [:show, :edit, :update, :destroy]
 
   def index
     if session[:cart]
@@ -9,8 +10,31 @@ class OrderProductsController < ApplicationController
     end
   end
 
+  # dont understand this, why is where empty? no id?
   def show
+    @order_products = OrderProduct.where()
+  end
+
+  def edit
+  end
+
+  def new
     @order_products = OrderProduct.new
+  end
+
+  def create 
+    @order_products.save
+    session[:cart] ||= []
+    session[:cart].push @order_product.id
+
+    output = {'status' => 'The item was added to your cart'}.to_json
+
+    format.html { redirect_to order_products_path, notice: 'The item was added to your cart'}
+    format.json { render :json => output }
+    else
+      format.html { render :new }
+      format.json {render json: @order_products.errors, status: :unprocessable_entity}
+    end 
   end
 
 end
