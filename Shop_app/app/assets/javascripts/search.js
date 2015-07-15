74e4284ff20fd44692ea8469e8915e0f8d8911e7
Laura Walpole <l.a.walpole@googlemail.com>
2015-07-15 15:46:45 +0100

    boxes level now

--- Shop_app/app/assets/javascripts/search.js
+++ Shop_app/app/assets/javascripts/search.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
-
-  //search function
-  //   $.ajax({
-  //   url: 'http://localhost:3000/',
-  //   dataType: 'json'
-  // }).done(function(response) {
-  //   console.log(response);
-  //   // debugger;
-  //   var FJS = FilterJS(response, '.yield_container', {
-  //     template: '.indivi_product',
-  //     search: {ele: '.form-control', fields: ['tags.name']}
-  //   });
-  // })
+    $.ajax({
+    url: 'http://localhost:3000/',
+    dataType: 'json'
+  }).done(function(response) {
+    console.log(response);
+    // debugger;
+    var FJS = FilterJS(response, '#final-product-search', {
+      template: '.indivi_product',
+      search: {ele: '.form-control', fields: ['tags.name']}
+    });
+  })
 
 
 });
