594f8bda42c4bd13de3b92c6fd3d58f3deb55925
Laura Walpole <l.a.walpole@googlemail.com>
2015-07-15 16:15:07 +0100

    working on connecting the button to the basket

--- Shop_app/app/controllers/products_controller.rb
+++ Shop_app/app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
 
    before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-  before_filter :check_if_admin, only: [:create, :new, :destroy, :update, :edit]
+   before_filter :check_if_admin, only: [:create, :new, :destroy, :update, :edit]
   
   def index
     @classes = ['one', 'two', 'three', 'four', 'five']
